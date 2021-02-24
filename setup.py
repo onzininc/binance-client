f63c05fe1ce9edb597f08464c16f452c7ea304f5
Benjamin Fagin <blouis@unquietcode.com>
2021-02-24 12:17:34 -0800

    client code

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_namespace_packages, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='binance-client',
     version='0.0',
     package_dir={
-        'src',
+        '': 'src',
     },
-    packages=['binance'],
+    packages=find_packages(where='src'),
     include_package_data=True,
     install_requires=[],
 )
